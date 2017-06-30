866636bd2dab8f4784b30a9e00ba0288baf39e54
younis hamalawy <younis.hamalawy@gmail.com>
2017-06-30 14:45:04 -0700

    Make curry function

--- argumentsExercises.js
+++ argumentsExercises.js
@@ -10,3 +10,44 @@ Function.prototype.myBind = function (context, ...bindArgs) {
     return this.apply(context, bindArgs.concat(callArgs));
   };
 };
+
+function curriedSum(numArgs) {
+  let numbers = [];
+  let _curriedSum = (num) => {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      return numbers.reduce((a, b) => a + b, 0);
+    } else {
+      return _curriedSum;
+    }
+  };
+
+  return _curriedSum;
+}
+
+// const csum = curriedSum(4);
+// console.log(csum(5)(30)(20)(1));
+
+
+Function.prototype.curry = function (numArgs, ...curryArgs) {
+  let things = curryArgs;
+  let _result = (...args) => {
+    things = things.concat(args);
+    if (things.length >= numArgs) {
+      return this(...things);
+    } else {
+      return _result;
+    }
+  };
+  return _result;
+};
+
+function addFive(one,t,th,f,fi) {
+  return one + t + th + f + fi;
+}
+let curriedaddFive = addFive.curry(5, 9);
+curriedaddFive(5,6);
+console.log(curriedaddFive(3,100,7));
+// let m = curriedSum.curry(4)();
+//
+// console.log(m(5)(30)(20)(1));
